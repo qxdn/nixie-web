cd371f81a2cd44a786c49315a4e1beced1bdfaba
Xu Qian <1464238196@qq.com>
2024-09-22 19:41:49 +0800

    fix title

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import React from "react";
 import DivergenceMeter from "./components/DivergenceMeter";
 import NixieClock from "./components/NixieClock";
 
 function App() {
+  const [mode, setMode] = React.useState<number>(0);
+
   return (
     <div className="w-screen h-screen bg-black">
       <div className="text-center w-full h-full justify-center items-center flex">
-       <DivergenceMeter />
+        {mode === 0 ? <NixieClock /> : <DivergenceMeter />}
       </div>
     </div>
   );
